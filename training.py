46c9c640da42db352fec360049a60de895b4c070
Geodego <geoffroydegournay@hotmail.com>
2022-08-09 12:21:08 +0200

    feat: training model done

--- training.py
+++ training.py
@@ -8,25 +8,37 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import json
 
-###################Load config.json and get path variables
-with open('config.json','r') as f:
-    config = json.load(f) 
+# Load config.json and get path variables
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-model_path = os.path.join(config['output_model_path']) 
+dataset_csv_path = os.path.join(config['output_folder_path'], 'finaldata.csv')
+model_path = os.path.join(config['output_model_path'], 'trainedmodel.pkl')
 
 
-#################Function for training the model
 def train_model():
-    
-    #use this logistic regression for training
-    LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
-                    intercept_scaling=1, l1_ratio=None, max_iter=100,
-                    multi_class='warn', n_jobs=None, penalty='l2',
-                    random_state=0, solver='liblinear', tol=0.0001, verbose=0,
-                    warm_start=False)
-    
-    #fit the logistic regression to your data
-    
-    #write the trained model to your workspace in a file called trainedmodel.pkl
+    """
+    Function for training the model
+    """
+    # use this logistic regression for training
+    model = LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
+                               intercept_scaling=1, l1_ratio=None, max_iter=100,
+                               multi_class='auto', n_jobs=None, penalty='l2',
+                               random_state=0, solver='liblinear', tol=0.0001, verbose=0,
+                               warm_start=False)
 
+    # fit the logistic regression to the data
+    X = pd.read_csv(dataset_csv_path).iloc[:, 1:]
+    y = X.pop('exited').values
+    model.fit(X, y)
+
+    # write the trained model to your workspace in a file called trainedmodel.pkl
+    try:
+        pickle.dump(model, open(model_path, 'wb'))
+    except FileNotFoundError:
+        os.mkdir(config['output_model_path'])
+        pickle.dump(model, open(model_path, 'wb'))
+
+
+if __name__ == '__main__':
+    train_model()
