ec17658e1ecc8a887223fc5bdd520ec32c43c0ee
Geodego <geoffroydegournay@hotmail.com>
2022-08-09 11:18:15 +0200

    feat: data ingestion done

--- ingestion.py
+++ ingestion.py
@@ -4,22 +4,44 @@ import os
 import json
 from datetime import datetime
 
-
-
-
-#############Load config.json and get input and output paths
-with open('config.json','r') as f:
-    config = json.load(f) 
+# Load config.json and get input and output paths
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
 input_folder_path = config['input_folder_path']
 output_folder_path = config['output_folder_path']
 
 
-
-#############Function for data ingestion
 def merge_multiple_dataframe():
-    #check for datasets, compile them together, and write to an output file
+    """
+    Function for data ingestion. Read all files in input_folder_path, concatenate them as a single dataframe, remove
+    duplicates  and save the data in output_folder_path/finaldata.csv
+    """
+    # check for datasets
+    filenames = next(os.walk(input_folder_path), (None, None, []))[2]  # [] if no file
 
+    # compile the datasets together
+    data_list = []
+    for file in filenames:
+        data_list.append(pd.read_csv(input_folder_path + '/' + file))
+    data = pd.concat(data_list)
+
+    # remove duplicates
+    data = data.drop_duplicates(ignore_index=True)
+
+    # Write to an output file
+    try:
+        data.to_csv(output_folder_path + '/' + 'finaldata.csv', index=False)
+    except FileNotFoundError:
+        os.mkdir(output_folder_path)
+        data.to_csv(output_folder_path + '/' + 'finaldata.csv', index=False)
+
+    # saving a record of the ingestion
+    with open(output_folder_path + '/' + 'ingestedfiles.txt', 'w') as f:
+        for file in filenames:
+            f.write(file + '\n')
+
+    return data
 
 
 if __name__ == '__main__':
