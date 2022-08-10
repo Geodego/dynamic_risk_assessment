3854a6ea27e956e148cf04cb1cfc013f435bb09a
Geodego <geoffroydegournay@hotmail.com>
2022-08-10 07:18:29 +0200

    feat: model  scoring done

--- scoring.py
+++ scoring.py
@@ -8,18 +8,35 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import json
 
+# Load config.json and get path variables
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+dataset_csv_path = os.path.join(config['output_folder_path'], 'finaldata.csv')
+test_data_path = os.path.join(config['test_data_path'], 'testdata.csv')
 
 
-#################Load config.json and get path variables
-with open('config.json','r') as f:
-    config = json.load(f) 
-
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path']) 
-
-
-#################Function for model scoring
 def score_model():
-    #this function should take a trained model, load test data, and calculate an F1 score for the model relative to the test data
-    #it should write the result to the latestscore.txt file
+    """
+    Function for model scoring
+    """
+    # load test_data and model
+    test_data = pd.read_csv(test_data_path)
+    model_path = os.path.join(config['output_model_path'], 'trainedmodel.pkl')
+    model = pickle.load(open(model_path, 'rb'))
+
+    # calculate f1 score
+    X = test_data.iloc[:, 1:]
+    y = X.pop('exited').values
+    y_pred = model.predict(X)
+    f1_score = metrics.f1_score(y, y_pred)
+
+    # save the result
+    output_path = os.path.join(config['output_model_path'], 'latestscore.txt')
+    with open(output_path, 'w') as f:
+        f.write(str(f1_score) + '\n')
+
+
+if __name__ == '__main__':
+    score_model()
 
