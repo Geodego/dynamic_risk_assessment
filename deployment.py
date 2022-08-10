e42ff767e1389857d4fcf5665c7400d87e0dd1f9
Geodego <geoffroydegournay@hotmail.com>
2022-08-10 09:32:26 +0200

    feat: deployment done

--- deployment.py
+++ deployment.py
@@ -1,27 +1,43 @@
-from flask import Flask, session, jsonify, request
-import pandas as pd
-import numpy as np
-import pickle
 import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 import json
 
+# Load config.json and correct path variable
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+dataset_csv_path = os.path.join(config['output_folder_path'])
+prod_deployment_path = os.path.join(config['prod_deployment_path'])
 
 
-##################Load config.json and correct path variable
-with open('config.json','r') as f:
-    config = json.load(f) 
-
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-prod_deployment_path = os.path.join(config['prod_deployment_path']) 
-
-
-####################function for deployment
 def store_model_into_pickle(model):
-    #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
-        
-        
-        
+    """
+    function for deployment, copy the latest pickle file, the latestscore.txt value, and the ingestedfiles.txt file
+    into the deployment directory
+    :param model:
+    """
+    # source paths
+    model_path = os.path.join(config['output_model_path'], model)
+    score_path = os.path.join(config['output_model_path'], 'latestscore.txt')
+    ingested_path = os.path.join(dataset_csv_path, 'ingestedfiles.txt')
+    sources = [model_path, score_path, ingested_path]
+
+    # target paths
+    # make sure the target directory exists
+    if not os.path.isdir(prod_deployment_path):
+        os.mkdir(prod_deployment_path)
+    target_model = os.path.join(prod_deployment_path, model)
+    target_score = os.path.join(prod_deployment_path, 'latestscore.txt')
+    target_ingested = os.path.join(prod_deployment_path, 'ingestedfiles.txt')
+    targets = [target_model, target_score, target_ingested]
+
+    for source, target in zip(sources, targets):
+        os.system(f'cp {source} {target}')
+
+
+if __name__ == '__main__':
+    model = 'trainedmodel.pkl'
+    store_model_into_pickle(model)
+
+
+
 
