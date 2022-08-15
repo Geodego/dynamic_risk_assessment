5573f9b1a40baac45606f98c9842aa686a7888ed
Geodego <geoffroydegournay@hotmail.com>
2022-08-15 06:58:18 +0200

    chore: logging added

--- reporting.py
+++ reporting.py
@@ -11,14 +11,18 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import json
 import os
+import logging
+
 from diagnostics import model_predictions
+
+logger = logging.getLogger(__name__)
 
 # Load config.json and get path variables
 with open('config.json', 'r') as f:
     config = json.load(f)
 
 
-def score_model():
+def get_confusion_matrix():
     """
     Reporting: calculate a confusion matrix using the test data and the deployed model. Write the confusion matrix to
     the directory specified in the output_model_path entry of the config.json file.
@@ -29,7 +33,8 @@ def score_model():
     y_true = pd.read_csv(test_data_path)['exited'].values
 
     # labels predicted by the model
-    y_pred = model_predictions(test_data_path)
+    data = pd.read_csv(test_data_path)
+    y_pred = model_predictions(data)
 
     # plot the confusion matrix
     confusion = metrics.confusion_matrix(y_true, y_pred)
@@ -42,7 +47,8 @@ def score_model():
     ax.xaxis.set_ticklabels(['False', 'True'])
     ax.yaxis.set_ticklabels(['False', 'True'])
     plt.savefig(os.path.join(config['output_model_path'], 'confusionmatrix.png'))
+    logger.info(f"confusion matrix saved in {config['output_model_path']}")
 
 
 if __name__ == '__main__':
-    score_model()
+    get_confusion_matrix()
